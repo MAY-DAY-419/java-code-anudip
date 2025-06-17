0b0e276357ade82e2b614211cc3dc8ff1bf10695
MAY-DAY-419 <mayankdantre16@gmail.com>
2025-06-17 22:31:04 +0530

    Create char-fun.java

---
+++ char-fun.java
@@ -0,0 +1,7 @@
+public class CharacterFunction {
+    public static void main(String[] args) {
+        System.out.println("Is Digit: " + Character.isDigit('82'));
+        System.out.println("Is Letter: " + Character.isLetter('p'));
+        System.out.println("To Upper Case: " + Character.toUpperCase('m'));
+    }
+}
